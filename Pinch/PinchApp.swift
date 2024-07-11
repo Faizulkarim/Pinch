9d4e721d6c6b820444878849c4c9798f27fcb400
Md Faizul karim <faizulkarim28@gmail.com>
2024-07-12 02:21:07 +0600

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Md Faizul karim on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
